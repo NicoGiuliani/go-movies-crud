944df743fe4b2cd96662e6a17847d13fb5084343
Nico Julian <NicoGiuliani@users.noreply.github.com>
2022-12-01 09:59:12 -0800

    Added routes

--- main.go
+++ main.go
@@ -21,3 +21,16 @@ type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname string `json:"lastname"`
 }
+
+var movies []Movie
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/movies", getMovies).Methods("GET")
+	router.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	router.HandleFunc("/movies", createMovie).Methods("POST")
+	router.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	router.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+}
+

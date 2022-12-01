8623effd24d933f08df3c65b52ee0257e0e9e13d
Nico Julian <NicoGiuliani@users.noreply.github.com>
2022-12-01 10:16:57 -0800

    Added functions

--- main.go
+++ main.go
@@ -24,13 +24,52 @@ type Director struct {
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+} 
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+} 
+
+funct getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder.Encode(item)
+			return
+		}
+	}
+}
+
+
 func main() {
 	router := mux.NewRouter()
 
+	movies = append(movies, Movie{
+		ID: "1", ISBN: "438227", Title: "Movie One", 
+		Director : &Director{Firstname: "John", Lastname: "Doe"}
+	})
+	movies = append(movies, Movie{
+		ID: "2", ISBN: "438228", Title: "Movie Two", 
+		Director : &Director{Firstname: "Bob", Lastname: "Smith"}
+	})
 	router.HandleFunc("/movies", getMovies).Methods("GET")
 	router.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	router.HandleFunc("/movies", createMovie).Methods("POST")
 	router.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	router.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
 

7a52e72f43b222342e04f6d591d8d9b4e7edf620
Nico Julian <NicoGiuliani@users.noreply.github.com>
2022-12-01 09:54:27 -0800

    Added Movie & Director structs

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"encoding/json"
 	"math/rand"
 	"net/http"
 	"strconv"
+	"github.com/gorilla/mux"
 )
 
+type Movie struct {
+	ID string `json:"id"`
+	ISBN string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
